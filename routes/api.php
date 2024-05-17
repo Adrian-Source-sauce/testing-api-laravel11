b765ebaa1b0f82c22042a8cccaf92929932632bd
Adrian-Source-sauce <ma469993@gmail.com>
2024-05-17 15:15:05 +0800

    edit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
+
+Route::get('/data', [PostController::class, 'index']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -9,3 +12,4 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
+
